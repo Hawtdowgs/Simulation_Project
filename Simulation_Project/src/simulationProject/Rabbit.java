0aaa56209ca7ae5aca64b42fa0ca8cd71d104fa7
Konner(Hawtdowgs) <ktf62642@gmail.com>
2020-02-19 10:01:14 -0600

    did some work

--- Simulation_Project/src/simulationProject/Rabbit.java
+++ Simulation_Project/src/simulationProject/Rabbit.java
@@ -4,11 +4,41 @@
  */
 package simulationProject;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Rabbit {
-
-	public static void main(String[] args) {
+	int inSpawn, age;
+	boolean alive, fed, roam, reproduce;
 	
-
+	public static int getRab() {
+		int spawnNum;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("how many rabbits?");
+		spawnNum = input.nextInt();
+		return spawnNum;
+	}
+	
+	
+	
+	public Rabbit() {
+		int x, y, inp;
+		int[][] ranSpawn;
+		Random r = new Random();
+		
+		inp = Rabbit.getRab();
+		
+		for(int i = 0; i < inp; i++) {
+			
+			x = r.nextInt(20 - 0 + 1) + 1;
+			y = r.nextInt(20 - 0 + 1) + 1;
+			
+			
+			
+		}
+		
+		
 	}
 
 }
