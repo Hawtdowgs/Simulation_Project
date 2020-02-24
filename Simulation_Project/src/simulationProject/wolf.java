11b80bccba0bbb30c2ffd9da28b6e250055fbeb7
Spencer <spencermolloy2715@gmail.com>
2020-02-24 09:48:28 -0600

    Yaya we make small progress

--- Simulation_Project/src/simulationProject/wolf.java
+++ Simulation_Project/src/simulationProject/wolf.java
@@ -3,24 +3,34 @@ import java.util.Random;
 import java.util.Scanner;
 public class wolf {
 
+	@SuppressWarnings("null")
 	public static void main(String[] args) {
 		//formatting, what's that?
 		Scanner input = new Scanner(System.in);
 		Random r = new Random();
 		
-		boolean[][] alive, sex, full, move;
+		boolean[][] wAlive = new boolean[20][20], sex, full, move;
 		int initial, pos1, pos2 ;
-		int[][] wolf = null;
-		System.out.println("how many wolves would you like to start with?");
+		int[][] wolf;
+		
+		//sets every space to have a "dead" wolf
+		for(int i = 0; i < 20; i ++) {
+			for(int j = 0; j < 20; j ++) {
+				wAlive[i][j] = false;
+			}
+		}
+		
+		System.out.println("How many wolves would you like to begin the simulation with?");
 		initial = input.nextInt();
 		
 		for(int i = 0; i < initial; i++) {
-			pos1 = r.nextInt(20-0+1)+0;
-			pos2 = r.nextInt(20-0+1)+0;
+			pos1 = r.nextInt(19-0+1)+0;
+			pos2 = r.nextInt(19-0+1)+0;
 			
-			System.out.print(" " + pos1 + " ");
-			System.out.println(" "+ pos2);
+			wAlive[pos1][pos2] = true;
+			// System.out.println(pos1 + " " + pos2 + " " + wAlive[pos1][pos2]);
 		}
 	}
+	
 
 }
