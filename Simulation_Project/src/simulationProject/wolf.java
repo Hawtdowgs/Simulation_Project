ee130ae12a594ff9c1944e7bd2505bfa8d855a44
Spencer <spencermolloy2715@gmail.com>
2020-02-24 10:03:00 -0600

    bigger change yaya

--- Simulation_Project/src/simulationProject/wolf.java
+++ Simulation_Project/src/simulationProject/wolf.java
@@ -5,32 +5,42 @@ public class wolf {
 
 	@SuppressWarnings("null")
 	public static void main(String[] args) {
-		//formatting, what's that?
 		Scanner input = new Scanner(System.in);
 		Random r = new Random();
 		
-		boolean[][] wAlive = new boolean[20][20], sex, full, move;
+		boolean[][] wAlive = new boolean[20][20], male = new boolean[20][20], full = new boolean[20][20],
+				move = new boolean[20][20];
 		int initial, pos1, pos2 ;
 		int[][] wolf;
 		
-		//sets every space to have a "dead" wolf
-		for(int i = 0; i < 20; i ++) {
-			for(int j = 0; j < 20; j ++) {
-				wAlive[i][j] = false;
-			}
-		}
+		wAlive = wolfSpawn();
 		
-		System.out.println("How many wolves would you like to begin the simulation with?");
-		initial = input.nextInt();
-		
-		for(int i = 0; i < initial; i++) {
-			pos1 = r.nextInt(19-0+1)+0;
-			pos2 = r.nextInt(19-0+1)+0;
-			
-			wAlive[pos1][pos2] = true;
-			// System.out.println(pos1 + " " + pos2 + " " + wAlive[pos1][pos2]);
-		}
 	}
 	
-
+	public static boolean[][] wolfSpawn(){
+		Scanner input = new Scanner(System.in);
+		Random r = new Random();
+		int pos1,pos2,initial;
+		boolean[][] alive = new boolean[20][20];
+		//sets every space to have a "dead" wolf
+				for(int i = 0; i < 20; i ++) {
+					for(int j = 0; j < 20; j ++) {
+						alive[i][j] = false;
+					}
+				}
+				
+				System.out.println("How many wolves would you like to begin the simulation with?");
+				initial = input.nextInt();
+				
+				for(int i = 0; i < initial; i++) {
+					pos1 = r.nextInt(19-0+1)+0;
+					pos2 = r.nextInt(19-0+1)+0;
+					
+					alive[pos1][pos2] = true;
+					// System.out.println(pos1 + " " + pos2 + " " + wAlive[pos1][pos2]);
+				}
+				return alive;
+	}
+	
+	
 }
