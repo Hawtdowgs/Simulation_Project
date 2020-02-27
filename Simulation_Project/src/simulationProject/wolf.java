bfdee7f9d5203e6748e0e08956047d49db1efb9c
Spencer <spencermolloy2715@gmail.com>
2020-02-27 10:04:07 -0600

    yaya

--- Simulation_Project/src/simulationProject/wolf.java
+++ Simulation_Project/src/simulationProject/wolf.java
@@ -2,71 +2,88 @@ package simulationProject;
 import java.util.Random;
 import java.util.Scanner;
 public class wolf {
-
+private boolean[][] wPosition, wSex, wFull, wMove;
+private int[][] wolf;
+private double rate;
+private int initial, sex, pos1, pos2;
 	@SuppressWarnings("null")
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+	public wolf() {
+		wPosition = new boolean[20][20];
+		wSex = new boolean[20][20];
+		wFull = new boolean[20][20];
+		wMove = new boolean[20][20];
+		wolf = new int[20][20];
+		rate = 0;
+		initial = 0;
+	}
+	public void wSetUp() {
 		Random r = new Random();
+		Scanner input = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 		
-		boolean[][] wAlive = new boolean[20][20], wSex = new boolean[20][20], full = new boolean[20][20],
-				move = new boolean[20][20], wNew;
-		int initial, pos1, pos2;
-		int[][] wolf;
+		boolean validRate = false, validStart;
 		
-		
-		wAlive = wolfSpawn(wSex);
-		wNew = wolfReproduce(wSex,wAlive,full);
-		
-		//this is for testing purposes
-		for(int i = 0; i < 20; i ++) {
-			for(int j = 0; j < 20; j ++) {
-				System.out.println(i + " " + j + " " + wAlive[i][j] + " " + wSex[i][j]);
+		//loops until a valid number has been entered
+		do {
+			System.out.println("How many wolves would you like to begin the simulation with?");
+			System.out.println("values between 0 and 400 inclusive are valid:");
+			initial = input.nextInt();
+			
+			if(initial < 0 ||  initial > 400) {
+				validStart = false;
+			}else {
+				validStart = true;
 			}
-		}
+		}while(validStart == false);
+		
+		//loops until a valid growth rate has been entered
+		do {
+			System.out.println("What it the reproductive rate of the wolves?");
+			System.out.println("values between 0 and 1 inclusive are valid:");
+			rate = input.nextDouble();
+			
+			if(rate < 0 || rate > 1) {
+				validRate = false;
+			}else {
+				validRate = true;
+			}
+		}while(validRate == false);
+		
+		
+		
 		
 	}
 	
 	@SuppressWarnings("null")
-	public static boolean[][] wolfSpawn(boolean[][] male){
+	public boolean[][] wSpawn(){
 		Scanner input = new Scanner(System.in);
 		Random r = new Random();
-		int pos1,pos2,initial,sex;
-		boolean[][] alive = new boolean[20][20];
+		
 		
 		//sets every space to a non wolf
 				for(int i = 0; i < 20; i ++) {
 					for(int j = 0; j < 20; j ++) {
-						alive[i][j] = false;
-						male[i][j] = false;
+						wPosition[i][j] = false;
+						wSex[i][j] = false;
 					}
 				}
-				
-				//asks and sets the starting number of wolves
-				System.out.println("How many wolves would you like to begin the simulation with?");
-				initial = input.nextInt();
 				
 				for(int i = 0; i < initial; i++) {
 					pos1 = r.nextInt(19-0+1)+0;
 					pos2 = r.nextInt(19-0+1)+0;
 					sex = r.nextInt(1-0+1)+0;
 					
-					alive[pos1][pos2] = true;
+					wPosition[pos1][pos2] = true;
 					
 					//gives the wolf a sex helping determine whether the wolf can reproduce
 					if(sex == 0) {
-						male[pos1][pos2] = true;
+						wSex[pos1][pos2] = true;
 					}else {
-						male[pos1][pos2] = false;
+						wSex[pos1][pos2] = false;
 					}
-					System.out.println(pos1 + " " + pos2 + " " + alive[pos1][pos2]  + " " + male[pos1][pos2]);
+					System.out.println(pos1 + " " + pos2 + " " + wPosition[pos1][pos2]  + " " + wSex[pos1][pos2]);
 				}
-				return alive;
-	}
-	
-	public static boolean[][] wolfReproduce(boolean[][]male, boolean[][]position, boolean[][]full){
-		Random r = new Random();
-		
-		
+				return wPosition;
 	}
 	
 }
